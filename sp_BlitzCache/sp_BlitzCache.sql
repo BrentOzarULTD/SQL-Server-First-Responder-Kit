5c5a21f7588034c98d9e7f38edcdfd5c638bc851
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2014-06-17 12:57:51 -0700

    Fixing duplicate line
    Must've been a bunk commit
    Fixes #366

--- sp_BlitzCache/sp_BlitzCache.sql
+++ sp_BlitzCache/sp_BlitzCache.sql
@@ -1658,7 +1658,6 @@ BEGIN
                 'Execution Plans',
                 'Nearly Parallel',
                 'http://brentozar.com/blitzcache/query-cost-near-cost-threshold-parallelism/',
-                'http://brentozar.com/blitzcache/queyr-cost-near-cost-threshold-parallelism/',
                 'Queries near the cost threshold for parallelism. These may go parallel when you least expect it.') ;
 
     IF EXISTS (SELECT 1/0
