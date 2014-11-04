84ba4a05002974b3e31b79ed212502663493e230
Jeremiah Peschka <jeremiah.peschka@gmail.com>
2014-11-04 14:51:38 -0800

    Bumping BlitzCache from develop

--- sp_BlitzCache/sp_BlitzCache.sql
+++ sp_BlitzCache/sp_BlitzCache.sql
@@ -60,6 +60,9 @@ KNOWN ISSUES:
 - @ignore_query_hashes and @only_query_hashes require a CSV list of hashes
   with no spaces between the hash values.
 
+v2.4.2 - 2014-11-04
+ - Hotfix - Randall Petty found a stray comma in the export_to_excel output.
+
 v2.4.1 - 2014-10-31
  - Hotfix - Denis Gobo pointed out that the global temp table names could
    conflict with everyone else's global temp table rates.
@@ -1631,6 +1634,7 @@ BEGIN
             LastExecutionTime AS [Last Execution],
             StatementStartOffset,
             StatementEndOffset,
+            COALESCE(SetOptions, '''') AS [SET Options],
             COALESCE(SetOptions, '''') AS [SET Options]
     FROM    ##bou_BlitzCacheProcs
     WHERE   1 = 1 ' + @nl
