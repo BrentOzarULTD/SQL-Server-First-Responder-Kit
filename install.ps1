4e1d9a68cd664d3ca208349d66b26a5250c94ccd
Selman AY <selmanay@gmail.com>
2016-06-24 17:39:51 +0300

    Update install.ps1

--- install.ps1
+++ install.ps1
@@ -1,32 +1,32 @@
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_AskBrent.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_Blitz.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_BlitzCache.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_BlitzIndex.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_BlitzTrace.sql -d "master" -b
-$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2014' -U 'sa' -P 'Password12!' -Database 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_AskBrent.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_Blitz.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_BlitzCache.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_BlitzIndex.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_BlitzTrace.sql -Database "master" -AbortOnError
+$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2014' -Username 'sa' -Password 'Password12!' -Databaseatabase 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
 if ($result -eq 'ReportServer')
 {
-  Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -U "sa" -P "Password12!" -i sp_BlitzRS.sql -d "master" -b
+  Invoke-Sqlcmd -ServerInstance "(local)\SQL2014" -Username "sa" -Password "Password12!" -InputFile sp_BlitzRS.sql -Database "master" -AbortOnError
 }
 
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_AskBrent.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_Blitz.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_BlitzCache.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_BlitzIndex.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_BlitzTrace.sql -d "master" -b
-$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2012SP1' -U 'sa' -P 'Password12!' -Database 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_AskBrent.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_Blitz.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_BlitzCache.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_BlitzIndex.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_BlitzTrace.sql -Database "master" -AbortOnError
+$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2012SP1' -Username 'sa' -Password 'Password12!' -Databaseatabase 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
 if ($result -eq 'ReportServer')
 {
-  Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -U "sa" -P "Password12!" -i sp_BlitzRS.sql -d "master" -b
+  Invoke-Sqlcmd -ServerInstance "(local)\SQL2012SP1" -Username "sa" -Password "Password12!" -InputFile sp_BlitzRS.sql -Database "master" -AbortOnError
 }
 
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_AskBrent.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_Blitz.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_BlitzCache.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_BlitzIndex.sql -d "master" -b
-Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_BlitzTrace.sql -d "master" -b
-$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2008R2SP2' -U 'sa' -P 'Password12!' -Database 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_AskBrent.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_Blitz.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_BlitzCache.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_BlitzIndex.sql -Database "master" -AbortOnError
+Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_BlitzTrace.sql -Database "master" -AbortOnError
+$result = Invoke-Sqlcmd -ServerInstance '(local)\SQL2008R2SP2' -Username 'sa' -Password 'Password12!' -Databaseatabase 'master' -Query "SELECT name FROM sys.databases where name = 'ReportServer'"
 if ($result -eq 'ReportServer')
 {
-  Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -U "sa" -P "Password12!" -i sp_BlitzRS.sql -d "master" -b
+  Invoke-Sqlcmd -ServerInstance "(local)\SQL2008R2SP2" -Username "sa" -Password "Password12!" -InputFile sp_BlitzRS.sql -Database "master" -AbortOnError
 }
