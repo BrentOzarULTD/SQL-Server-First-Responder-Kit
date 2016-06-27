033283f83a1805fd45a0ecaf6d25b3916f65a031
Selman AY <selmanay@gmail.com>
2016-06-27 09:33:48 +0300

    Update sp_BlitzIndex.sql

--- sp_BlitzIndex.sql
+++ sp_BlitzIndex.sql
@@ -28,7 +28,6 @@ ALTER PROCEDURE dbo.sp_BlitzIndex
     @OutputSchemaName NVARCHAR(256) = NULL ,
     @OutputTableName NVARCHAR(256) = NULL ,
 	@Help TINYINT = 0,
-	@selman_ay int = 0,
 	@VersionDate DATETIME = NULL OUTPUT
 AS
 SET NOCOUNT ON;
