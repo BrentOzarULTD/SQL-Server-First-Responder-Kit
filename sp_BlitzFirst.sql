3ed21e55e1628d46b05f71d40426fbea7644965b
BlitzErik <erik@brentozar.com>
2016-07-18 11:13:02 -0400

    Tidying up
    Everything looks good here.

--- sp_BlitzFirst.sql
+++ sp_BlitzFirst.sql
@@ -810,7 +810,7 @@ BEGIN
 		        CASE @Seconds WHEN 0 THEN 0 ELSE SUM(owt.wait_duration_ms) OVER (PARTITION BY owt.wait_type)
 					 - CASE WHEN @Seconds = 0 THEN 0 ELSE (@Seconds * 1000) END END AS sum_wait_time_ms,
 				0 AS sum_signal_wait_time_ms,
-				CASE @Seconds WHEN 0 THEN 0 ELSE 2 END AS sum_waiting_tasks
+				0 AS sum_waiting_tasks
 			FROM    sys.dm_os_waiting_tasks owt
 			WHERE owt.session_id > 50
 			AND owt.wait_duration_ms >= CASE @Seconds WHEN 0 THEN 0 ELSE @Seconds * 1000 END
@@ -2597,7 +2597,6 @@ BEGIN
                 ORDER BY [Wait Time (Seconds)] DESC;
                 END;
 
-
             -------------------------
             --What happened: #FileStats
             -------------------------
@@ -2960,6 +2959,7 @@ END /* ELSE IF @OutputType = 'SCHEMA' */
 
 SET NOCOUNT OFF;
 GO
+
 
 
 /* How to run it:
