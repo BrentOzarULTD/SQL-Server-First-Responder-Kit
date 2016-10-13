9419e6d66ef931f7d18fc9b204e860994efc5d08
BlitzErik <erik@brentozar.com>
2016-10-13 12:58:13 -0400

    Adds partition compression level detail back
    Hooray.

--- sp_BlitzIndex.sql
+++ sp_BlitzIndex.sql
@@ -146,6 +146,9 @@ IF OBJECT_ID('tempdb..#IndexCreateTsql') IS NOT NULL
 IF OBJECT_ID('tempdb..#DatabaseList') IS NOT NULL 
     DROP TABLE #DatabaseList;
 
+IF OBJECT_ID('tempdb..#PartitionCompressionInfo') IS NOT NULL 
+    DROP TABLE #PartitionCompressionInfo;
+
         RAISERROR (N'Create temp tables.',0,1) WITH NOWAIT;
         CREATE TABLE #BlitzIndexResults
             (
@@ -507,6 +510,11 @@ IF OBJECT_ID('tempdb..#DatabaseList') IS NOT NULL
 
         CREATE TABLE #DatabaseList (
 			DatabaseName NVARCHAR(256)
+        )
+
+		CREATE TABLE #PartitionCompressionInfo (
+			[index_sanity_id] INT NULL,
+			[partition_compression_detail] VARCHAR(8000) NULL
         )
 
 IF @GetAllDatabases = 1
@@ -1353,7 +1361,49 @@ BEGIN TRY
                 AS create_tsql
         FROM #IndexSanity
         WHERE database_id = @DatabaseID;
-                    
+ 
+ ;WITH    [maps]
+          AS ( SELECT   
+                        index_sanity_id,
+						partition_number,
+                        data_compression_desc,
+                        partition_number - ROW_NUMBER() OVER (PARTITION BY ips.index_sanity_id, data_compression_desc ORDER BY partition_number ) AS [rN]
+               FROM     #IndexPartitionSanity ips
+				),
+        [grps]
+          AS ( SELECT   MIN([maps].[partition_number]) AS [MinKey] ,
+                        MAX([maps].[partition_number]) AS [MaxKey] ,
+						index_sanity_id,
+						maps.data_compression_desc
+               FROM     [maps]
+               GROUP BY [maps].[rN], index_sanity_id, maps.data_compression_desc)
+    INSERT #PartitionCompressionInfo
+            (index_sanity_id, partition_compression_detail)
+	SELECT DISTINCT grps.index_sanity_id , SUBSTRING((  STUFF((SELECT ', ' + ' Partition'
+                                                + CASE WHEN [grps2].[MinKey] < [grps2].[MaxKey]
+                                                       THEN +'s '
+                                                            + CAST([grps2].[MinKey] AS VARCHAR)
+                                                            + ' - '
+                                                            + CAST([grps2].[MaxKey] AS VARCHAR)
+                                                            + ' use ' + grps2.data_compression_desc
+                                                       ELSE ' '
+                                                            + CAST([grps2].[MinKey] AS VARCHAR)
+                                                            + ' uses '  + grps2.data_compression_desc
+                                                  END AS [Partitions]
+                                         FROM   [grps] AS grps2
+										 WHERE grps2.index_sanity_id = grps.index_sanity_id
+										 ORDER BY grps2.MinKey, grps2.MaxKey
+                                FOR     XML PATH('') ,
+                                            TYPE 
+						).[value]('.', 'VARCHAR(MAX)'), 1, 1, '') ), 0, 8000) AS [partition_compression_detail]
+	FROM grps;
+
+	UPDATE sz
+	SET sz.data_compression_desc = pci.partition_compression_detail
+	FROM #IndexSanitySize sz
+	JOIN #PartitionCompressionInfo AS pci
+	ON pci.index_sanity_id = sz.index_sanity_id		
+                  
     END
 END TRY
 BEGIN CATCH
