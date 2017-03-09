e86edf6167c31a79480af0e428c927bf0743a282
Jeff Rosenberg <jeff.rosenberg@olson.com>
2017-03-08 21:40:29 -0600

    Changed @MoveOption variable from nvarchar(1000) to nvarchar(MAX)

--- DatabaseRestore.sql
+++ DatabaseRestore.sql
@@ -92,7 +92,7 @@ DECLARE @FileListParameters TABLE
 INSERT INTO @FileListParameters
 EXEC ('RESTORE FILELISTONLY FROM DISK='''+@BackupPathFull + @LastFullBackup+'''');
 
-DECLARE @MoveOption AS NVARCHAR(1000)= '';
+DECLARE @MoveOption AS NVARCHAR(MAX)= '';
 
 IF @MoveFiles = 1
 BEGIN
