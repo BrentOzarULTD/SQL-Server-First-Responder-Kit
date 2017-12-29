30d7e16edb7dfb07a62c08347fa7d01545dd07f0
russellhart <34937557+russellhart@users.noreply.github.com>
2017-12-29 11:01:22 +0000

    Fix heap active and inactive checks
    When a row is added to sys.dm_db_index_usage_stats it returns zeros for user columns, not nulls. This causes tables to be incorrectly reported as active.

--- sp_Blitz.sql
+++ sp_Blitz.sql
@@ -5262,7 +5262,7 @@ IF @ProductVersionMajor >= 10
 		  INNER JOIN [?].sys.partitions p ON i.object_id = p.object_id AND i.index_id = p.index_id
 		  INNER JOIN sys.databases sd ON sd.name = ''?''
 		  LEFT OUTER JOIN [?].sys.dm_db_index_usage_stats ius ON i.object_id = ius.object_id AND i.index_id = ius.index_id AND ius.database_id = sd.database_id
-		  WHERE i.type_desc = ''HEAP'' AND AND COALESCE(ius.user_seeks,0) = 0 AND COALESCE(ius.user_scans,0) = 0 AND COALESCE(ius.user_lookups,0) = 0 AND COALESCE(ius.user_updates,0) = 0 
+		  WHERE i.type_desc = ''HEAP'' AND COALESCE(ius.user_seeks,0) = 0 AND COALESCE(ius.user_scans,0) = 0 AND COALESCE(ius.user_lookups,0) = 0 AND COALESCE(ius.user_updates,0) = 0 
 		  AND sd.name <> ''tempdb'' AND sd.name <> ''DWDiagnostics'' AND o.is_ms_shipped = 0 AND o.type <> ''S'' OPTION (RECOMPILE)';
 							END;
 
