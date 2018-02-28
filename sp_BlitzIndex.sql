d4ea98bc8e00a2b64a1d492f6cf442e3895311ef
BlitzErik <erik@brentozar.com>
2018-02-28 06:28:20 -0500

    Widen table definition
    Makes the `details` column a MAX.
    
    Fixes #1449

--- sp_BlitzIndex.sql
+++ sp_BlitzIndex.sql
@@ -177,7 +177,7 @@ IF OBJECT_ID('tempdb..#TemporalTables') IS NOT NULL
               finding VARCHAR(200) NOT NULL,
               [database_name] VARCHAR(200) NULL,
               URL VARCHAR(200) NOT NULL,
-              details NVARCHAR(4000) NOT NULL,
+              details NVARCHAR(MAX) NOT NULL,
               index_definition NVARCHAR(MAX) NOT NULL,
               secret_columns NVARCHAR(MAX) NULL,
               index_usage_summary NVARCHAR(MAX) NULL,
