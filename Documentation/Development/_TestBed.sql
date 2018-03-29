2edac46fc3c624a7ea2710b2f8d0046775739339
BlitzErik <BlitzErik@users.noreply.github.com>
2018-03-29 15:43:47 -0400

    Adds some more lines to testbed
    This helps me test in different spots.

--- Documentation/Development/_TestBed.sql
+++ Documentation/Development/_TestBed.sql
@@ -7,6 +7,11 @@ EXEC sp_Blitz @CheckUserDatabaseObjects = 1, @CheckServerInfo = 1, @Debug = 1
 
 EXEC sp_Blitz @CheckUserDatabaseObjects = 1, @CheckServerInfo = 1, @Debug = 2
 
+/*BlitzWho*/
+
+EXEC dbo.sp_BlitzWho @ExpertMode = 1, @Debug = 1
+
+EXEC dbo.sp_BlitzWho @ExpertMode = 0, @Debug = 1
 
 /*BlitzFirst*/
 EXEC sp_BlitzFirst @Seconds = 5, @ExpertMode = 1
@@ -18,13 +23,21 @@ EXEC sp_BlitzFirst @Seconds = 5, @ExpertMode = 1, @ShowSleepingSPIDs = 1
 /*BlitzIndex*/
 EXEC sp_BlitzIndex @GetAllDatabases = 1, @Mode = 4
 
---EXEC sp_BlitzIndex @DatabaseName = 'StackOverflow', @Mode = 4
+EXEC sp_BlitzIndex @DatabaseName = 'StackOverflow', @Mode = 4
+
+EXEC sp_BlitzIndex @DatabaseName = N'StackOverflow2010ಠ_ಠ', @Mode = 4
+
+EXEC sp_BlitzIndex @DatabaseName = N'ಠ_ಠ', @Mode = 4
 
 EXEC sp_BlitzIndex @DatabaseName = N'¯\_(ツ)_/¯', @Mode = 4
 
---EXEC sp_BlitzIndex @DatabaseName = 'StackOverflow', @Mode = 4, @SkipPartitions = 0, @SkipStatistics = 0
+EXEC sp_BlitzIndex @DatabaseName = 'StackOverflow', @Mode = 4, @SkipPartitions = 0, @SkipStatistics = 0
 
 EXEC sp_BlitzIndex @DatabaseName = N'¯\_(ツ)_/¯', @Mode = 4, @SkipPartitions = 0, @SkipStatistics = 0
+
+EXEC sp_BlitzIndex @DatabaseName = N'StackOverflow2010ಠ_ಠ', @Mode = 4, @SkipPartitions = 0, @SkipStatistics = 0
+
+EXEC sp_BlitzIndex @DatabaseName = N'ಠ_ಠ', @Mode = 4, @SkipPartitions = 0, @SkipStatistics = 0
 
 EXEC sp_BlitzIndex @GetAllDatabases = 1, @Mode = 1
 
@@ -41,6 +54,8 @@ EXEC sp_BlitzCache @SortOrder = 'all avg'
 EXEC sp_BlitzCache @MinimumExecutionCount = 10
 
 EXEC sp_BlitzCache @DatabaseName = N'¯\_(ツ)_/¯'
+
+EXEC sp_BlitzCache @DatabaseName = N'StackOverflow2010ಠ_ಠ'
 
 EXEC sp_BlitzCache @OutputDatabaseName = 'OutputTest', @OutputSchemaName = 'dbo', @OutputTableName = 'BlitzCache'
 
