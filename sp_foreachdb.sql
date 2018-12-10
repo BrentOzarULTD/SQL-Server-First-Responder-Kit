ba68d2bc9cedbcc6bfd542ba2cbc7385b83aa6a8
pwsqldba <45744127+pwsqldba@users.noreply.github.com>
2018-12-10 11:56:03 +1100

    Update help info
    Change "this script will restore" to "execute command against"

--- sp_foreachdb.sql
+++ sp_foreachdb.sql
@@ -47,7 +47,7 @@ IF @Help = 1
 		/*
 			sp_foreachdb from http://FirstResponderKit.org
 			
-			This script will restore a database from a given file path.
+			This script will execute a given command against all, or user-specified, available databases on an instance.
 		
 			To learn more, visit http://FirstResponderKit.org where you can download new
 			versions for free, watch training videos on how it works, get more info on
