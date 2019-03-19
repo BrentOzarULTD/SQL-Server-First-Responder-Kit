37e8b1c8f75e5a94cfbd25cc4a5e91127a9e4a55
Matthew Monroe <matthew.monroe@pnnl.gov>
2019-03-19 02:41:36 -0700

    #1988 sp_BlitzIndex INSERT column order bug (#1989)
    * #1952 sp_BlitzIndex Fix INSERT query column order
    
    * #1988 sp_BlitzIndex Update the column order of the INSERT statement and SELECT items to match the CREATE TABLE statement
    
    Move columns Drop_Tsql and Create_Tsql to after index_name for consistency with the CREATE TABLE statement

--- sp_BlitzIndex.sql
+++ sp_BlitzIndex.sql
@@ -4637,8 +4637,6 @@ BEGIN;
 											[create_date], 
 											[modify_date], 
 											[more_info],
-                                            [Drop_Tsql],
-                                            [Create_Tsql], 
 											[display_order]
 										)
 									SELECT ''@@@RunID@@@'',
@@ -4650,6 +4648,18 @@ BEGIN;
 										i.[schema_name] AS [Schema Name], 
 										i.[object_name] AS [Object Name], 
 										ISNULL(i.index_name, '''') AS [Index Name],
+                                        CASE 
+						                    WHEN i.is_primary_key = 1 AND i.index_definition <> ''[HEAP]''
+							                    THEN N''-ALTER TABLE '' + QUOTENAME(i.[schema_name]) + N''.'' + QUOTENAME(i.[object_name]) +
+							                         N'' DROP CONSTRAINT '' + QUOTENAME(i.index_name) + N'';''
+						                    WHEN i.is_primary_key = 0 AND i.index_definition <> ''[HEAP]''
+						                        THEN N''--DROP INDEX ''+ QUOTENAME(i.index_name) + N'' ON '' + 
+							                         QUOTENAME(i.[schema_name]) + N''.'' + QUOTENAME(i.[object_name]) + N'';''
+						                ELSE N''''
+						                END AS [Drop TSQL],
+					                    CASE 
+						                    WHEN i.index_definition = ''[HEAP]'' THEN N''''
+					                            ELSE N''--'' + ict.create_tsql END AS [Create TSQL],
 										CAST(i.index_id AS NVARCHAR(10))AS [Index ID],
 										db_schema_object_indexid AS [Details: schema.table.index(indexid)], 
 										CASE    WHEN index_id IN ( 1, 0 ) THEN ''TABLE''
@@ -4712,18 +4722,6 @@ BEGIN;
 										i.create_date AS [Create Date],
 										i.modify_date AS [Modify Date],
 										more_info AS [More Info],
-                                        CASE 
-						                    WHEN i.is_primary_key = 1 AND i.index_definition <> ''[HEAP]''
-							                    THEN N''-ALTER TABLE '' + QUOTENAME(i.[schema_name]) + N''.'' + QUOTENAME(i.[object_name]) +
-							                         N'' DROP CONSTRAINT '' + QUOTENAME(i.index_name) + N'';''
-						                    WHEN i.is_primary_key = 0 AND i.index_definition <> ''[HEAP]''
-						                        THEN N''--DROP INDEX ''+ QUOTENAME(i.index_name) + N'' ON '' + 
-							                         QUOTENAME(i.[schema_name]) + N''.'' + QUOTENAME(i.[object_name]) + N'';''
-						                ELSE N''''
-						                END AS [Drop TSQL],
-					                    CASE 
-						                    WHEN i.index_definition = ''[HEAP]'' THEN N''''
-					                            ELSE N''--'' + ict.create_tsql END AS [Create TSQL],  
 										1 AS [Display Order]
 									FROM #IndexSanity AS i
 									LEFT JOIN #IndexSanitySize AS sz ON i.index_sanity_id = sz.index_sanity_id
