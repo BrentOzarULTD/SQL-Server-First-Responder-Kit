a1374fa67e052ff564890c2e738e4db4266d745a
Brent Ozar <brento@brentozar.com>
2020-05-11 06:05:11 -0700

    #2373 removing non-readable character in sp_Blitz
    Closes #2373.

--- sp_Blitz.sql
+++ sp_Blitz.sql
@@ -5863,7 +5863,7 @@ IF @ProductVersionMajor >= 10
 							       'SQL Server Update May Fail' AS Finding,
 							       'https://desertdba.com/failovers-cant-serve-two-masters/' AS URL,
 							       'This database has a file with a logical name of ''master'', which can break SQL Server updates. Rename it in SSMS by right-clicking on the database, go into Properties, and rename the file. Takes effect instantly.' AS details
-							FROM master.sys.master_files f
+							FROM master.sys.master_files f
 							WHERE (f.name = N'master')
 							  AND f.database_id > 4
 							  AND db_name(f.database_id) <> 'master'; /* Thanks Michaels3 for catching this */
