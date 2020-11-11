d814e4cfe83b4533ec41e16229e293c632993a79
John McCall <john@lowlydba.com>
2020-11-11 15:48:48 -0500

    Add space after colon to match "Reads: " format

--- sp_BlitzIndex.sql
+++ sp_BlitzIndex.sql
@@ -334,7 +334,7 @@ IF OBJECT_ID('tempdb..#Ignore_Databases') IS NOT NULL
 							+ N') '
 						ELSE N' '
 						END 
-					+ N'Writes:' + 
+					+ N'Writes: ' + 
 					REPLACE(CONVERT(NVARCHAR(30),CAST(user_updates AS MONEY), 1), N'.00', N'')
 				END /* First "end" is about is_spatial */,
 				[more_info] AS 
