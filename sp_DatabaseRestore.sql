3745088163c0330680feda2bcc1fee302b53c742
Brent Ozar <brento@brentozar.com>
2022-10-13 09:43:07 -0400

    2022-10-13 October Release
    Bumping version numbers and dates.

--- sp_DatabaseRestore.sql
+++ sp_DatabaseRestore.sql
@@ -42,7 +42,7 @@ SET STATISTICS XML OFF;
 
 /*Versioning details*/
 
-SELECT @Version = '8.10', @VersionDate = '20220718';
+SELECT @Version = '8.11', @VersionDate = '20221013';
 
 IF(@VersionCheckMode = 1)
 BEGIN
